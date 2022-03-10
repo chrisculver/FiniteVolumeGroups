e09dc7945ff8a0d3aa5efadef22cb8dff66c91f3
chrisculver <chris.cb.culver@gmail.com>
2022-03-10 09:16:22 +0000

    continuing to work on characters

--- tests/test_o2_and_oh2.py
+++ tests/test_o2_and_oh2.py
@@ -3,11 +3,13 @@ from FiniteVolumeGroups.cubic import *
 import math
 import sys
 
+
 def test_o2():
     o2 = O2()
     assert len(o2.elements) == 48
 
-    class_elems = lambda name : sum(elem.conjugacy_class == name for elem in o2.elements)
+    def class_elems(name): return sum(
+        elem.conjugacy_class == name for elem in o2.elements)
 
     assert class_elems("E") == 1
     assert class_elems("Ebar") == 1
@@ -20,10 +22,11 @@ def test_o2():
 
 
 def test_o2h():
-  o2h=O2h()
-  assert(len(o2h.elements)==96)
+  o2h = O2h()
+  assert(len(o2h.elements) == 96)
 
-  class_elems = lambda name : sum(elem.conjugacy_class == name for elem in o2h.elements)
+  def class_elems(name): return sum(
+      elem.conjugacy_class == name for elem in o2h.elements)
 
   assert class_elems("E") == 1
   assert class_elems("Ebar") == 1
@@ -46,26 +49,26 @@ def test_o2h():
 def test_o2_characters():
     o2 = O2()
 
-    a1_characters = {    "E":  1, "Ebar":  1,    "C2xyz":  1,     "C3":  1,
+    a1_characters = {"E":  1, "Ebar":  1,    "C2xyz":  1,     "C3":  1,
                      "C3bar":  1,   "C4":  1, "C4bar":  1, "C2diag":  1}
-    a2_characters = {    "E":  1, "Ebar":  1,    "C2xyz":  1,     "C3":  1,
+    a2_characters = {"E":  1, "Ebar":  1,    "C2xyz":  1,     "C3":  1,
                      "C3bar":  1,   "C4": -1, "C4bar": -1, "C2diag": -1}
 
-    e_characters =  {    "E":  2, "Ebar":  2,    "C2xyz":  2,     "C3": -1,
-                     "C3bar": -1,   "C4":  0, "C4bar":  0, "C2diag":  0}
+    e_characters = {"E":  2, "Ebar":  2,    "C2xyz":  2,     "C3": -1,
+                    "C3bar": -1,   "C4":  0, "C4bar":  0, "C2diag":  0}
 
-    t1_characters = {    "E":  3, "Ebar":  3,    "C2xyz": -1,     "C3":  0,
+    t1_characters = {"E":  3, "Ebar":  3,    "C2xyz": -1,     "C3":  0,
                      "C3bar":  0,   "C4":  1, "C4bar":  1, "C2diag": -1}
-    t2_characters = {    "E":  3, "Ebar":  3,    "C2xyz": -1,     "C3":  0,
+    t2_characters = {"E":  3, "Ebar":  3,    "C2xyz": -1,     "C3":  0,
                      "C3bar":  0,   "C4": -1, "C4bar": -1, "C2diag":  1}
 
-    g1_characters = {    "E":  2, "Ebar":  -2,    "C2xyz":  0,     "C3":  1,
-                     "C3bar":  -1,   "C4":  math.sqrt(2), "C4bar":  -math.sqrt(2), "C2diag": 0}
-    g2_characters = {    "E":  2, "Ebar":  -2,    "C2xyz":  0,     "C3":  1,
-                     "C3bar":  -1,   "C4": -math.sqrt(2), "C4bar":  math.sqrt(2), "C2diag": 0}
+    g1_characters = {"E":  2, "Ebar": -2,    "C2xyz":  0,     "C3":  1,
+                     "C3bar": -1,   "C4":  math.sqrt(2), "C4bar": -math.sqrt(2), "C2diag": 0}
+    g2_characters = {"E":  2, "Ebar": -2,    "C2xyz":  0,     "C3":  1,
+                     "C3bar": -1,   "C4": -math.sqrt(2), "C4bar":  math.sqrt(2), "C2diag": 0}
 
-    h_characters =  {    "E":  4, "Ebar":  -4,    "C2xyz":  0,     "C3": -1,
-                     "C3bar":  1,   "C4":   0, "C4bar":  0, "C2diag": 0}
+    h_characters = {"E":  4, "Ebar": -4,    "C2xyz":  0,     "C3": -1,
+                    "C3bar":  1,   "C4":   0, "C4bar":  0, "C2diag": 0}
 
     all_characters = {"A1": a1_characters, "A2": a2_characters,
                       "E": e_characters,
@@ -73,19 +76,19 @@ def test_o2_characters():
                       "G1": g1_characters, "G2": g2_characters,
                       "H": h_characters}
 
-
-    elements_of_class = lambda name: list(filter(lambda x: x.conjugacy_class == name, o2.elements))
+    def elements_of_class(name): return list(
+        filter(lambda x: x.conjugacy_class == name, o2.elements))
 
     for irrep, characters in all_characters.items():
       for conj, char in characters.items():
         elems = elements_of_class(conj)
-        print("Irrep = {},  conj={}".format(irrep,conj))
+        print("Irrep = {},  conj={}".format(irrep, conj))
         for elem in elems:
           print(elem.irreps[irrep])
           print(elem.identifier)
           print(char)
           print(elem.irreps[irrep].trace())
-          trace=complex(elem.irreps[irrep].trace())
+          trace = complex(elem.irreps[irrep].trace())
           assert np.isclose(trace.real, char)
           assert np.isclose(trace.imag, 0.)
 
@@ -93,39 +96,117 @@ def test_o2_characters():
 def test_o2h_characters():
     o2h = O2h()
     #characters from arxiv 1706.00262
-    a1g_characters = {    "E":  1, "Ebar":  1,    "C2xyz":  1,     "C3":  1,
+    a1g_characters = {"E":  1, "Ebar":  1,    "C2xyz":  1,     "C3":  1,
                       "C3bar":  1,   "C4":  1, "C4bar":  1, "C2diag":  1,
                       "iE":  1, "iEbar":  1,    "iC2xyz":  1,     "iC3":  1,
                       "iC3bar":  1,   "iC4":  1, "iC4bar":  1, "iC2diag":  1}
 
-    a1u_characters = {    "E":  1, "Ebar":  1,    "C2xyz":  1,     "C3":  1,
+    a1u_characters = {"E":  1, "Ebar":  1,    "C2xyz":  1,     "C3":  1,
                       "C3bar":  1,   "C4":  1, "C4bar":  1, "C2diag":  1,
-                      "iE":  -1, "iEbar":  -1,    "iC2xyz":  -1,     "iC3":  -1,
-                      "iC3bar":  -1,   "iC4":  -1, "iC4bar":  -1, "iC2diag":  -1}
+                      "iE": -1, "iEbar": -1,    "iC2xyz": -1,     "iC3": -1,
+                      "iC3bar": -1,   "iC4": -1, "iC4bar": -1, "iC2diag": -1}
 
-    a2g_characters = {
+    a2g_characters = {"E":  1, "Ebar":  1,    "C2xyz":  1,     "C3":  1,
+                      "C3bar":  1,   "C4":  1, "C4bar":  1, "C2diag":  1,
+                      "iE": -1, "iEbar": -1,    "iC2xyz": -1,     "iC3": -1,
+                      "iC3bar": -1,   "iC4": -1, "iC4bar": -1, "iC2diag": -1}
 
-    }
+    a2u_characters = {"E":  1, "Ebar":  1,    "C2xyz":  1,     "C3":  1,
+                      "C3bar":  1,   "C4":  1, "C4bar":  1, "C2diag":  1,
+                      "iE": -1, "iEbar": -1,    "iC2xyz": -1,     "iC3": -1,
+                      "iC3bar": -1,   "iC4": -1, "iC4bar": -1, "iC2diag": -1}
 
-    a2u_characters = {
-    
-    }
+    eg_characters = {"E":  1, "Ebar":  1,    "C2xyz":  1,     "C3":  1,
+                     "C3bar":  1,   "C4":  1, "C4bar":  1, "C2diag":  1,
+                     "iE": -1, "iEbar": -1,    "iC2xyz": -1,     "iC3": -1,
+                     "iC3bar": -1,   "iC4": -1, "iC4bar": -1, "iC2diag": -1}
+
+    eu_characters = {"E":  1, "Ebar":  1,    "C2xyz":  1,     "C3":  1,
+                     "C3bar":  1,   "C4":  1, "C4bar":  1, "C2diag":  1,
+                     "iE": -1, "iEbar": -1,    "iC2xyz": -1,     "iC3": -1,
+                     "iC3bar": -1,   "iC4": -1, "iC4bar": -1, "iC2diag": -1}
+
+    t1g_characters = {"E":  1, "Ebar":  1,    "C2xyz":  1,     "C3":  1,
+                      "C3bar":  1,   "C4":  1, "C4bar":  1, "C2diag":  1,
+                      "iE": -1, "iEbar": -1,    "iC2xyz": -1,     "iC3": -1,
+                      "iC3bar": -1,   "iC4": -1, "iC4bar": -1, "iC2diag": -1
+                      }
+
+    t1u_characters = {"E":  1, "Ebar":  1,    "C2xyz":  1,     "C3":  1,
+                      "C3bar":  1,   "C4":  1, "C4bar":  1, "C2diag":  1,
+                      "iE": -1, "iEbar": -1,    "iC2xyz": -1,     "iC3": -1,
+                      "iC3bar": -1,   "iC4": -1, "iC4bar": -1, "iC2diag": -1
+                      }
+
+    t2g_characters = {"E":  1, "Ebar":  1,    "C2xyz":  1,     "C3":  1,
+                      "C3bar":  1,   "C4":  1, "C4bar":  1, "C2diag":  1,
+                      "iE": -1, "iEbar": -1,    "iC2xyz": -1,     "iC3": -1,
+                      "iC3bar": -1,   "iC4": -1, "iC4bar": -1, "iC2diag": -1
+                      }
+
+    t2u_characters = {"E":  1, "Ebar":  1,    "C2xyz":  1,     "C3":  1,
+                      "C3bar":  1,   "C4":  1, "C4bar":  1, "C2diag":  1,
+                      "iE": -1, "iEbar": -1,    "iC2xyz": -1,     "iC3": -1,
+                      "iC3bar": -1,   "iC4": -1, "iC4bar": -1, "iC2diag": -1
+                      }
+
+    g1u_characters = {"E":  1, "Ebar":  1,    "C2xyz":  1,     "C3":  1,
+                      "C3bar":  1,   "C4":  1, "C4bar":  1, "C2diag":  1,
+                      "iE": -1, "iEbar": -1,    "iC2xyz": -1,     "iC3": -1,
+                      "iC3bar": -1,   "iC4": -1, "iC4bar": -1, "iC2diag": -1
+                      }
+    g1g_characters = {"E":  1, "Ebar":  1,    "C2xyz":  1,     "C3":  1,
+                      "C3bar":  1,   "C4":  1, "C4bar":  1, "C2diag":  1,
+                      "iE": -1, "iEbar": -1,    "iC2xyz": -1,     "iC3": -1,
+                      "iC3bar": -1,   "iC4": -1, "iC4bar": -1, "iC2diag": -1
+                      }
+
+    g2u_characters = {"E":  1, "Ebar":  1,    "C2xyz":  1,     "C3":  1,
+                      "C3bar":  1,   "C4":  1, "C4bar":  1, "C2diag":  1,
+                      "iE": -1, "iEbar": -1,    "iC2xyz": -1,     "iC3": -1,
+                      "iC3bar": -1,   "iC4": -1, "iC4bar": -1, "iC2diag": -1
+                      }
+
+    g2g_characters = {"E":  1, "Ebar":  1,    "C2xyz":  1,     "C3":  1,
+                      "C3bar":  1,   "C4":  1, "C4bar":  1, "C2diag":  1,
+                      "iE": -1, "iEbar": -1,    "iC2xyz": -1,     "iC3": -1,
+                      "iC3bar": -1,   "iC4": -1, "iC4bar": -1, "iC2diag": -1
+                      }
+
+    hu_characters = {"E":  1, "Ebar":  1,    "C2xyz":  1,     "C3":  1,
+                     "C3bar":  1,   "C4":  1, "C4bar":  1, "C2diag":  1,
+                     "iE": -1, "iEbar": -1,    "iC2xyz": -1,     "iC3": -1,
+                     "iC3bar": -1,   "iC4": -1, "iC4bar": -1, "iC2diag": -1
+                     }
+
+    hg_characters = {"E":  1, "Ebar":  1,    "C2xyz":  1,     "C3":  1,
+                     "C3bar":  1,   "C4":  1, "C4bar":  1, "C2diag":  1,
+                     "iE": -1, "iEbar": -1,    "iC2xyz": -1,     "iC3": -1,
+                     "iC3bar": -1,   "iC4": -1, "iC4bar": -1, "iC2diag": -1
+                     }
 
     all_characters = {"A1g": a1g_characters, "A1u": a1u_characters,
-                      "A2g": a2g_characters, "A2u": a2u_characters}
+                      "A2g": a2g_characters, "A2u": a2u_characters,
+                      "Eg": eg_characters, "Eu": eu_characters,
+                      "T1g": t1g_characters, "T1u": t2u_characters,
+                      "T2g": t2g_characters, "T2u": t2u_characters,
+                      "G1g": g1g_characters, "G1u": g1u_characters,
+                      "G2g": g2g_characters, "G2u": g2u_characters,
+                      "Hg": hg_characters, "Hu": hu_characters
+                      }
 
-
-    elements_of_class = lambda name: list(filter(lambda x: x.conjugacy_class == name, o2h.elements))
+    def elements_of_class(name): return list(
+        filter(lambda x: x.conjugacy_class == name, o2h.elements))
 
     for irrep, characters in all_characters.items():
       for conj, char in characters.items():
         elems = elements_of_class(conj)
-        print("Irrep = {},  conj={}".format(irrep,conj))
+        print("Irrep = {},  conj={}".format(irrep, conj))
         for elem in elems:
           print(elem.irreps[irrep])
           print(elem.identifier)
           print(char)
           print(elem.irreps[irrep].trace())
-          trace=complex(elem.irreps[irrep].trace())
+          trace = complex(elem.irreps[irrep].trace())
           assert np.isclose(trace.real, char)
           assert np.isclose(trace.imag, 0.)
