358fdb8347b28a4cef8cd2b3433f78b019d0e8f3
chrisculver <chris.cb.culver@gmail.com>
2022-03-21 23:17:30 +0000

    added exception to rep checks

--- FiniteVolumeGroups/representation_checks.py
+++ FiniteVolumeGroups/representation_checks.py
@@ -3,15 +3,15 @@ import numpy as np
 
 def is_valid_rep(rep):
   res = True
-  if not is_closed(rep):
-    res = False
-  if not is_associative(rep):
-    res = False
   if not is_nonzero(rep):
     res = False
   if not has_identity(rep):
     res = False
+  if not is_closed(rep):
+    res = False
   if not has_inverses(rep):
+    res = False
+  if not is_associative(rep):
     res = False
 
   return res
@@ -19,23 +19,27 @@ def is_valid_rep(rep):
 
 def is_closed(rep):
   res = True
-  for g1 in rep:
-      for g2 in rep:
+  for i1, g1 in enumerate(rep):
+      for i2, g2 in enumerate(rep):
           prod = np.matmul(g1, g2)
           if not any(np.allclose(prod, g) for g in rep):
               res = False
+              raise AssertionError(
+                "rep not closed for elem indices {} {}".format(i1, i2))
   return res
 
 
 def is_associative(rep):
   res = True
-  for a in rep:
-      for b in rep:
-          for c in rep:
+  for i1, a in enumerate(rep):
+      for i2, b in enumerate(rep):
+          for i3, c in enumerate(rep):
               lhs = np.matmul(np.matmul(a, b), c)
               rhs = np.matmul(a, np.matmul(b, c))
               if not np.allclose(lhs, rhs):
                   res = False
+                  raise AssertionError(
+                    "rep not associative for elem indices {} {} {}".format(i1, i2, i3))
   return res
 
 
@@ -45,25 +49,29 @@ def has_identity(rep):
   for g in rep:
     if np.allclose(g, id):
       res = True
+  if not res:
+    raise AssertionError("rep doesn't have identity")
   return res
 
 
 def has_inverses(rep):
   res = True
-  for g1 in rep:
+  for i1, g1 in enumerate(rep):
       has_inverse = False
-      for g2 in rep:
+      for i2, g2 in enumerate(rep):
           if np.allclose(np.matmul(g1, g2), np.identity(len(rep[0]))):
               has_inverse = True
       if not has_inverse:
           res = False
+          raise AssertionError("rep missing inverse for element {}".format(i1))
   return res
 
 
 def is_nonzero(rep):
   res = True
   zero = np.zeros((len(rep[0]), len(rep[0])))
-  for g in rep:
+  for i, g in enumerate(rep):
     if np.allclose(g, zero):
       res = False
+      raise AssertionError("element {} is zero, aka non-invertible".format(i))
   return res
